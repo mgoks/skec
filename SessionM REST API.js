ed0a00defafaed7f76305e60cd19525db3405232
Murat Goksel <34871949+mgoks@users.noreply.github.com>
2019-03-14 14:04:30 -0400

    Improve formatting

--- SessionM REST API.js
+++ SessionM REST API.js
@@ -2,17 +2,17 @@
 
 	Platform.Load("core", "1.1.1");
 	
-	var accessToken = "";
-	var url = "";
-	var auth = 'Bearer ' + accessToken;
-	var request = new Script.Util.HttpRequest(url);
-	request.emptyContentHandling = 0;
-	request.retries = 2;
-	request.continueOnError = true;
-	request.contentType = "application/json";
-	request.setHeader = ("Authorization", auth);
-	request.method = "PUT";
-	request.postData = {
+	var accessToken 		= "";
+	var url 			= "";
+	var auth 			= 'Bearer ' + accessToken;
+	var request 			= new Script.Util.HttpRequest(url);
+	request.emptyContentHandling 	= 0;
+	request.retries 		= 2;
+	request.continueOnError 	= true;
+	request.contentType 		= "application/json";
+	request.setHeader 		= ("Authorization", auth);
+	request.method 			= "PUT";
+	request.postData 		= {
  		“user”:{
 			“external_id”	: “%%=V(@SubscriberKey)=%%“,
 			“email”			: “%%=V(@SubscriberEmail)=%%“,
@@ -21,8 +21,8 @@
 			“zip”			: “%%=V(@ZipCode)=%%“,
   		}
 	};
+
 	var response = request.send();
-	
 	Platform.Response.Write("Content:<br/>" + response.content);
 	Platform.Response.Write("Return header: " + response.headers["returnHeader"]);
 
