c65f69840daffe2c217042c8583e1151fca6a4d6
Murat Goksel <34871949+mgoks@users.noreply.github.com>
2019-03-14 16:22:38 -0400

    Add Basic Authorization credentials to request

--- SessionM REST API.js
+++ SessionM REST API.js
@@ -2,19 +2,21 @@
 
 	Platform.Load("core", "1.1.1");
 	
-	var accessToken 		= "";
-	var url 			= "";
-	var auth 			= 'Bearer ' + accessToken;
+	var key				= "INSERT_API_KEY_HERE";
+	var secret			= "INSERT_API_SECRET_HERE";
+	var externalId			= "%%=V(@SubscriberKey)=%%";
+
+	var url 			= "https://api-skechers-usa.stg-sessionm.com/priv/v1/apps/" + key + "/external/users/" + externalId;
 	var request 			= new Script.Util.HttpRequest(url);
 	request.emptyContentHandling 	= 0;
 	request.retries 		= 2;
 	request.continueOnError 	= true;
 	request.contentType 		= "application/json";
-	request.setHeader 		= ("Authorization", auth);
+	request.setHeader 		= ("Authorization", "Basic " + key + ":" + secret);
 	request.method 			= "PUT";
 	request.postData 		= {
  		“user”:{
-			“external_id”	: “%%=V(@SubscriberKey)=%%“,
+			“external_id”	: externalId,
 			“email”		: “%%=V(@SubscriberEmail)=%%“,
 			“gender”	: “%%=V(@Gender)=%%“,
 			“dob”		: “%%=V(@DOB)=%%“,
