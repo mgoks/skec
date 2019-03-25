9e09a6d8a931c190a5a7e01d50a5e6c0762a2bf8
Murat Goksel <34871949+mgoks@users.noreply.github.com>
2019-03-25 16:39:35 -0400

    Update SessionM REST API.js

--- SessionM REST API.js
+++ SessionM REST API.js
@@ -1,31 +1,31 @@
 <script runat=server>
 
-	Platform.Load("core", "1.1.1");
-	
-	var key				= "INSERT_API_KEY_HERE";
-	var secret			= "INSERT_API_SECRET_HERE";
-	var externalId		= "%%=V(@SubscriberKey)=%%";
+    Platform.Load("core", "1.1.1");
+    
+    var key         = "INSERT_API_KEY_HERE";
+    var secret      = "INSERT_API_SECRET_HERE";
+    var externalId  = "%%=V(@SubscriberKey)=%%";
 
-	var url 			= "https://api-skechers-usa.stg-sessionm.com/priv/v1/apps/" + key + "/external/users/" + externalId;
-	var request 			= new Script.Util.HttpRequest(url);
-	request.emptyContentHandling 	= 0;
-	request.retries 		= 2;
-	request.continueOnError 	= true;
-	request.contentType 		= "application/json";
-	request.setHeader 		= ("Authorization", "Basic " + key + ":" + secret);
-	request.method 			= "PUT";
-	request.postData 		= {
- 		“user”:{
-			“external_id”	: externalId,
-			“email”		: “%%=V(@SubscriberEmail)=%%“,
-			“gender”	: “%%=V(@Gender)=%%“,
-			“dob”		: “%%=V(@DOB)=%%“,
-			“zip”		: “%%=V(@ZipCode)=%%“,
-  		}
-	};
+    var url = "https://api-skechers-usa.stg-sessionm.com/priv/v1/apps/" + key + "/external/users/" + externalId;
+    var request                     = new Script.Util.HttpRequest(url);
+    request.emptyContentHandling    = 0;
+    request.retries                 = 2;
+    request.continueOnError         = true;
+    request.contentType             = "application/json";
+    request.setHeader               = ("Authorization", "Basic " + key + ":" + secret);
+    request.method                  = "PUT";
+    request.postData                = {
+        “user”:{
+            “external_id”   : externalId,
+            “email”         : “%%=V(@SubscriberEmail)=%%“,
+            “gender”        : “%%=V(@Gender)=%%“,
+            “dob”           : “%%=V(@DOB)=%%“,
+            “zip”           : “%%=V(@ZipCode)=%%“,
+        }
+    };
 
-	var response = request.send();
-	Platform.Response.Write("Content:<br/>" + response.content);
-	Platform.Response.Write("Return header: " + response.headers["returnHeader"]);
+    var response = request.send();
+    Platform.Response.Write("Content:<br/>" + response.content);
+    Platform.Response.Write("Return header: " + response.headers["returnHeader"]);
 
 </script>
